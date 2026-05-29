40fa0f41f8b9b79b941d2c483ab0586b00accb7a
Sara Tibaduiza <stibaduiza483@uan.edu.co>
2026-05-29 02:22:23 -0500

    Commit parcial de nueva informacion y logica

---
+++ app/src/main/java/Cita.kt
@@ -0,0 +1,10 @@
+package com.example.menteconecta
+
+data class Cita(
+    val id: String = "",
+    val pacienteNombre: String = "",
+    val doctorNombre: String = "",
+    val fecha: String = "",
+    val hora: String = "",
+    val motivo: String = ""
+)
